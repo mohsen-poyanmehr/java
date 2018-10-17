48aebaa2708a2b0b063e53332b64ede497b43b4c
mohsen POYANMEHR-khodam <moh_phar@yahoo.com>
2018-10-17 15:43:52 +0200

    first commit modify by myself

---
+++ Applet.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Applet extends JApplet{
+  public void paint(Graphics g){
+    super.paint(g);
+    g.drawString("coucoucccccccccccccccccccccccccccccccc",100,300);
+  }
+}
